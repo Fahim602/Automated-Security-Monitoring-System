a5d3407d7cf0a5d2b7ad559cdb3b33664e685c1b
Fahim602 <ali.fahim2704@gmail.com>
2025-01-02 09:30:28 -0500

    Created structure and started on networkscan script

---
+++ monitoring/networkscan.py
@@ -0,0 +1,3 @@
+from scapy.all import ARP, Ether, srp
+# this script will use ARP requests to identify devices in the local network
+# ARP (Address Resolution Protocol) maps IP addresses of a device to their MAC Address
